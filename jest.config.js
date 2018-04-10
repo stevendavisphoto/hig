f38da73f5e8ec7323ede7093f310d51b8d979075
Eric Fung <ericf@carbonfive.com>
2018-04-10 15:10:51 -0700

    WIP: move test config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   collectCoverageFrom: [
     "**/src/**/*.{js,jsx}"
   ],
-  coverageDirectory: "<rootDir>/packages/storybook/coverage",
+  coverageDirectory: "<rootDir>/coverage",
   coveragePathIgnorePatterns: [
     "node_modules",
     "packages\/vanilla",
@@ -20,13 +20,12 @@ module.exports = {
     }
   },
   moduleNameMapper: {
-    "\\.(css|scss|svg)$": "<rootDir>/packages/storybook/support/jest/fileMock.js"
+    "\\.(css|scss|svg)$": "<rootDir>/support/jest/fileMock.js"
   },
   modulePaths: ["node_modules"],
   moduleFileExtensions: ["js", "jsx", "json"],
-  rootDir: "../../",
   setupFiles: ["raf/polyfill"],
-  setupTestFrameworkScriptFile: "<rootDir>/packages/storybook/support/jest/setupTests.js",
+  setupTestFrameworkScriptFile: "<rootDir>/support/jest/setupTests.js",
   testPathIgnorePatterns: ["<rootDir>/packages/vanilla"],
   unmockedModulePathPatterns: ["node_modules"]
 };
